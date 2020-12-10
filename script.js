098ba1dfcb93914fbc2772f68e0e397c18f2b1be
aviki <toldi.victoria@gmail.com>
2020-12-10 23:55:51 +0100

    implemented scroll effect & added more sections

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
+const navLogo = document.querySelector("#navbar-logo");
 
 // Display Mobile Menu
 
@@ -9,3 +10,48 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener("click", mobileMenu);
+
+// Show active menu when scrolling
+
+const highLightMenu = () => {
+  const elem = document.querySelector(".highlight");
+  const homeMenu = document.querySelector("#home-page");
+  const aboutMenu = document.querySelector("#about-page");
+  const learnMenu = document.querySelector("#learn-page");
+  const communityMenu = document.querySelector("#community-page");
+  const downloadMenu = document.querySelector("#download-page");
+  let scrollPos = window.scrollY;
+
+  // adds "highlight class to my menu items"
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add("highlight");
+    homeMenu.classList.remove("highlight");
+    learnMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    learnMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    communityMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 3200) {
+    communityMenu.classList.add("highlight");
+    learnMenu.classList.remove("highlight");
+    downloadMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 4000) {
+    downloadMenu.classList.add("highlight");
+    communityMenu.classList.remove("highlight");
+    return;
+  }
+
+  if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove("highlight");
+  }
+};
+
+window.addEventListener("scroll", highLightMenu);
+window.addEventListener("click", highLightMenu);
