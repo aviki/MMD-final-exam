f103148b6aeb5e655f5afb5b5f14b994d9b971de
aviki <toldi.victoria@gmail.com>
2020-12-12 00:05:40 +0100

    sript and style for sign up modal

--- script.js
+++ script.js
@@ -83,3 +83,17 @@ var typed = new Typewriter(".typing", {
   autoStart: true,
   loop: true,
 });
+
+// Sign up modal script
+
+var modalBtn = document.querySelector("#signup");
+var modalBg = document.querySelector(".modal-bg");
+var modalClose = document.querySelector(".modal-close");
+
+modalBtn.addEventListener("click", function () {
+  modalBg.classList.add("bg-active");
+});
+
+modalClose.addEventListener("click", function () {
+  modalBg.classList.remove("bg-active");
+});
