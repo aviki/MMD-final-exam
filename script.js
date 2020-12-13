d3d41e868d8491f302c3ae60507072acdff374c4
aviki <toldi.victoria@gmail.com>
2020-12-13 03:16:18 +0100

    implemented form animation

--- script.js
+++ script.js
@@ -97,3 +97,24 @@ modalBtn.addEventListener("click", function () {
 modalClose.addEventListener("click", function () {
   modalBg.classList.remove("bg-active");
 });
+
+// Form animation
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunction() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunction() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunction);
+  input.addEventListener("blur", blurFunction);
+});
